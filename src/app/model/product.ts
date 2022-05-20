b032585c9699fd717b45ac1d3af11e532ccda866
ShivaChallagonda <shivachallagonda@gmail.com>
2022-05-20 20:49:52 +0530

    first commit

---
+++ src/app/model/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+  id?: string;
+  productName: String = '';
+  type: String = '';
+  description: String = '';
+  productimage: String = '';
+  price: number = 0;
+  rating: number = 4;
+}
